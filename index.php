701d83e6c634fdb8cf5c980e89045bc6ee854a6d
ispykenny <kenny.krosky@gmail.com>
2019-08-21 08:09:04 -0700

    updates to font

--- index.php
+++ index.php
@@ -22,6 +22,24 @@
 			</div>
 		</div>
 	</section>
+
+	<section class="default-block">
+		<div class="inner">
+			<div class="content-area">
+				<h4 class="section-hdr">Section Element</h4>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis aut quam voluptatum voluptatem, itaque consectetur dicta totam tempore eligendi velit, eum similique optio animi amet ea dolores saepe laboriosam suscipit!</p>
+			</div>
+		</div>
+	</section>
+
+	<section class="default-block">
+		<div class="inner">
+			<div class="content-area">
+				<h4 class="section-hdr">Section Element</h4>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis aut quam voluptatum voluptatem, itaque consectetur dicta totam tempore eligendi velit, eum similique optio animi amet ea dolores saepe laboriosam suscipit!</p>
+			</div>
+		</div>
+	</section>
 </div>
 
 <?php get_footer(); ?>
